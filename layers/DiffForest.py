0856d746c1dad7a903831ec68b3fe68e71c00f9b
alex.kouzemtchenko <alex@whiteops.com>
2016-08-31 11:48:38 -0400

    Fix my silliness

--- layers/DiffForest.py
+++ layers/DiffForest.py
@@ -51,8 +51,7 @@ class DiffForest(Layer):
         leaf_p_e = []
         for w_d, w_l in zip(self.w_d_ensemble, self.w_l_ensemble):
 
-            #Move sigmoid into output space 0-1
-            decision_p = (K.sigmoid((K.dot(x, w_d))) + 1) /2
+            decision_p = K.sigmoid((K.dot(x, w_d)))
             leaf_p = K.softmax(w_l)
 
             decision_p_comp = 1 - decision_p
